159e7d7e577c0468a8e750dc486f7f03414a7e88
Edward Louw <edward.louw@resy.com>
2025-03-22 04:59:34 -0400

    Updated SPM support for Tuist integration.
    (cherry picked from commit 35e1e351f26fddfdc2f2cad702c29a9d8774eb22)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -6,9 +6,10 @@ let package = Package(
     name: "libPhoneNumber",
     platforms: [
         .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .macCatalyst(.v13),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
     ],
     products: [
         .library(
@@ -23,6 +24,9 @@ let package = Package(
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath("Internal")
+            ],
+            linkerSettings: [
+                .linkedFramework("Contacts", .when(platforms: [.iOS, .macOS, .macCatalyst, .watchOS])),
             ]
         ),
         .testTarget(
